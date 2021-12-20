69f389aa6226f2a463e131b52ce237aafff2640f
JENNY0222 <10756023@ntub.edu.tw>
2021-12-20 11:06:00 +0800

    feat(coffees): add show pages

--- coffees/urls.py
+++ coffees/urls.py
@@ -3,7 +3,12 @@ from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path
 from . import views
+
+
 app_name = 'coffees'
 urlpatterns = [
+    # ex: /coffees/
     path('', views.index, name='index'),
+    # ex: /coffees/1/
+    path('<int:pk>/', views.show, name='show'),
 ]
--- coffees/views.py
+++ coffees/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from coffees.models import Coffee
 
@@ -9,3 +9,8 @@ from coffees.models import Coffee
 def index(request):
     coffees = Coffee.objects.all()
     return render(request, 'coffees/index.html', {'coffees': coffees})
+
+
+def show(request, pk):
+    coffee = get_object_or_404(Coffee, pk=pk)
+    return render(request, 'coffees/show.html', {'coffee': coffee})
