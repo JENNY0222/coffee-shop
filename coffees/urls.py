e506dbf5543e35a0672a1ca2c26a0ccf5e95d8e1
JENNY0222 <10756023@ntub.edu.tw>
2021-12-20 10:40:50 +0800

    feat(coffees):add index page

--- coffees/urls.py
+++ coffees/urls.py
@@ -3,7 +3,7 @@ from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path
 from . import views
-
+app_name = 'coffees'
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
 ]
